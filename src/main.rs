11451cc4c9d60209eac2ac5ba072b3c86ee7b845
Jonathan Gerber <jlgerber@gmail.com>
2018-09-04 08:12:54 -0700

    handles undo

--- src/main.rs
+++ src/main.rs
@@ -7,15 +7,15 @@ use std::io;
 use std::fs::{self, DirEntry, rename};
 use rayon::prelude::*;
 
-fn fix_dirs() {
-    env::args().skip(1).for_each(|x| {
-        if fix_dir(x.as_str()).is_err() {
-            println!("problem renaming {}", x.as_str());
+fn fix_dirs(pathstr: &str, replace: &str, replace_with: &str) {
+    //env::args().skip(1).for_each(|x| {
+        if fix_dir(pathstr, replace, replace_with).is_err() {
+            println!("problem renaming {}", pathstr);
         };
-    });
+    //});
 }
 
-fn fix_dir(pathstr: &str) -> io::Result<()> {
+fn fix_dir(pathstr: &str, replace: &str, replace_with: &str) -> io::Result<()> {
     let dir = Path::new(pathstr);
     if dir.is_dir() {
         fs::read_dir(dir)?
@@ -26,23 +26,22 @@ fn fix_dir(pathstr: &str) -> io::Result<()> {
         .par_iter()
         .for_each(|entry| {
             let mut path = entry.path();
-           
                 // the heart of it
             if let Some(pathstr) = path.clone().to_str() {
-                let replacement = pathstr.replace(" ", "_");
-                let replacement = Path::new(replacement.as_str());
+                let replacement_string = pathstr.replace(replace, replace_with);
+                let replacement = Path::new(replacement_string.as_str());
                 if rename(path.clone(),replacement).is_err() {
                     println!("renaming {} failed", pathstr);
                 } else {
-                    if pathstr.contains(" ") { 
-                        println!("renamed {}",pathstr);
+                    if pathstr.contains(replace) { 
+                        println!("mv {} => {}",pathstr, replacement_string);
                         path = replacement.to_path_buf();
                     }
                 };
             }
             if path.is_dir() {
                 // recurse
-                if fix_dir(path.clone().to_str().expect("unable to convert")).is_err() {
+                if fix_dir(path.clone().to_str().expect("unable to convert"), replace, replace_with).is_err() {
                     println!("fixdir failed on {}", path.to_str().expect("unable to convert"));
                 }
             }
@@ -50,7 +49,12 @@ fn fix_dir(pathstr: &str) -> io::Result<()> {
     }
     Ok(())
 }
-
+use std::process;
 fn main() {
-    fix_dirs();
+    let mut enviter = env::args().skip(1);
+    //enviter.next();
+    let path = enviter.next().unwrap_or_else(||{println!("incorrect usage"); process::exit(1); });
+    let skipchar = enviter.next().unwrap_or(" ".to_string());
+    let withchar = enviter.next().unwrap_or("_".to_string());
+    fix_dirs(path.as_str(), skipchar.as_str(), withchar.as_str());
 }
